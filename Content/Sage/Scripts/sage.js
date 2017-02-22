306bf29c4dddc7bbb351a015a04571a28884594a
Mike McBain <mike.mcbain@outlook.com>
2017-02-22 11:23:31 +0000

    Adding existing product filter module

--- Content/Sage/Scripts/sage.js
+++ Content/Sage/Scripts/sage.js
@@ -560,251 +560,145 @@ sageApp.modules.register("productFeature", function ($) {
 /*global sageApp */
 
 sageApp.modules.register("productFilter", function ($) {
-  "use strict";
+    "use strict";
 
-  var moduleClass = "filter-module-content:not(.partners)",
-    filterButtonsClass = "product-filter-icon-wrapper",
-    activeClass = "active",
-    productResultsListClass = "filter-module-results-list",
-    productPanelClass = "product-panel",
-    filterTableClass = "filter-module-results-table",
-    productPanelDisabledClass = "panel-disabled",
-    sageCompareControl = {},
-    isFiltered = false;
+    var moduleClass = "filter-module-content:not(.partners)",
+        filterButtonsClass = "product-filter-icon-wrapper",
+        activeClass = "active",
+        activeFiltersArray = [],
+        productsArray = [],
+        sageCompareControl;
 
-  function init() {
-    //bind all events to document for delagation to traget product-filter-icon-wrapper
-    $(document).on("click", "." + moduleClass + " ." + filterButtonsClass, function (e) {
-      e.preventDefault();
-      filterIconClicked(e.currentTarget);
-    });
-    $(document).on("click", "." + moduleClass + " ." + "product-select-icon", function (e) {
-      e.preventDefault();
-      selectIconClicked(e.currentTarget);
-    });
-    $(document).on("click", "." + moduleClass + " ." + "mob-trigger", function (e) {
-      e.preventDefault();
-      mobileProductClicked(e.currentTarget);
-    });
-    $(document).on("click", "." + moduleClass + " ." + "navigation", function (e) {
-      e.preventDefault();
-      navigationClicked(e.currentTarget);
-    });
+    function init() {
+        //bind all events to document for delagation to traget product-filter-icon-wrapper
+          $(document).on("click", "." + moduleClass + " ." + filterButtonsClass, function (e) {
+              e.preventDefault();
+              filterIconClicked(e.currentTarget);
+          });
 
-    if (!$("div." + productResultsListClass).length)
-      return;
+          $(document).on("click", "." + moduleClass + " ." + "product-select-icon", function (e) {
+              e.preventDefault();
+              updateCompare(e.currentTarget);
+          });
 
-    initProdSlider();
-  }
+          setTimeout(initProdSlider, 700);
 
-  function toggleNavigationDisabled() {
-    var filteredProducts = $("." + moduleClass + " .mobile-table-header > .mob-trigger.isFiltered"),
-      numFilteredProducts = filteredProducts.length,
-      activeProductposition = 1,
-      tmpCounter = 1;
-
-    filteredProducts.each(function () {
-      if ($(this).hasClass(activeClass)) {
-        activeProductposition = tmpCounter;
-      }
-      tmpCounter++;
-    });
-
-    $("." + moduleClass + " ." + "navigation").removeClass("disabled");
-
-    if (activeProductposition === numFilteredProducts) {
-      $("." + moduleClass + " ." + "navigation.btnNext").addClass("disabled");
-    }
-    if (activeProductposition === 1) {
-      $("." + moduleClass + " ." + "navigation.btnPrevious").addClass("disabled");
-    }
-  }
-
-  function navigationClicked(clickedItem) {
-    var currentProduct = $("." + moduleClass + " .mobile-table-header > .mob-trigger.isFiltered.active");
-
-    if ($(clickedItem).hasClass("btnNext")) {
-
-      currentProduct.nextAll(".isFiltered").first().trigger("click");
-    } else {
-
-      currentProduct.prevAll(".isFiltered").first().trigger("click");
     }
 
-    toggleNavigationDisabled();
-  }
-
-  function mobileProductClicked(clickedItem) {
-    var targetID = $(clickedItem).attr("data-prod-target");
-    $("." + moduleClass + " ." + "mob-trigger").removeClass(activeClass);
-    $(clickedItem).addClass(activeClass);
-    $("." + moduleClass + " ." + "slider-mobile").removeClass(activeClass);
-    $("#" + targetID).addClass(activeClass);
-  }
-
-  function mobileProductUnClicked(clickedItem) {
-    var targetID = $(clickedItem).attr("data-prod-target");
-    $(clickedItem).removeClass(activeClass);
-    $("#" + targetID).removeClass(activeClass);
-    var numActive = $("." + moduleClass + " ." + "mob-trigger.isFiltered.active").length;
-
-    if (numActive === 0) {
-      $("." + moduleClass + " ." + "mob-trigger.isFiltered").first().addClass(activeClass);
-    }
-  }
-
-  function updateFilterTable() {
-    var activeProducts = $("." + productResultsListClass + " ." + productPanelClass + " .icon-select-text" + "." + activeClass),
-      numActiveProducts = activeProducts.length,
-      allProducts = $("." + productResultsListClass + " ." + productPanelClass),
-      allProductClasses = [],
-      selectedProductClasses = [];
-
-    activeProducts.each(function () {
-      var productActive = $(this).closest("." + productPanelClass).attr("data-filter-product");
-      selectedProductClasses.push(productActive);
-    });
-
-    allProducts.each(function () {
-      var productActive = $(this).attr("data-filter-product");
-      allProductClasses.push(productActive);
-    });
-
-    var testProd = -1;
-    for (var i = allProductClasses.length; i--;) {
-      testProd = selectedProductClasses.indexOf(allProductClasses[i]);
-      if (testProd === -1) {
-        $("." + filterTableClass).removeClass(allProductClasses[i]);
-      } else {
-        $("." + filterTableClass).addClass(allProductClasses[i]);
-      }
+    function filterIconClicked(clickedItem) {
+        var clickedFilter = $(clickedItem).attr("data-filter-attribute");
+        if (clickedFilter) {
+            $(clickedItem).toggleClass(activeClass);
+            updateFilters(clickedFilter);
+        }  
     }
 
-    var slickFilterString = "";
-    for (var i2 = selectedProductClasses.length; i2--;) {
-      if (i2 !== 0) {
-        slickFilterString = slickFilterString + "div." + selectedProductClasses[i2].replace("-active", "-mob") + ", ";
-      } else {
-        slickFilterString = slickFilterString + "div." + selectedProductClasses[i2].replace("-active", "-mob");
-      }
-    }
-
-    if (numActiveProducts > 0) {
-      $("." + filterTableClass).removeClass("hidden");
-      filterProductsMobile(slickFilterString);
-
-    } else {
-      $("." + filterTableClass).addClass("hidden");
-      removeProductFilters();
-    }
-  }
-
-  function selectIconClicked(clickedItem) {
-    $(clickedItem).toggleClass(activeClass);
-    $(clickedItem).closest(".panel").next(".icon-select-text").toggleClass(activeClass);
-
-    if ($(clickedItem).closest(".panel").next(".icon-select-text").hasClass(activeClass)) {
-      mobileProductClicked(".mobile-table-header" + " ." + $(clickedItem).closest(".product-panel").attr("data-prod-target"));
-    } else {
-      mobileProductUnClicked(".mobile-table-header" + " ." + $(clickedItem).closest(".product-panel").attr("data-prod-target"));
-    }
-    updateFilterTable();
-    toggleNavigationDisabled();
-  }
-
-  function filterIconClicked(clickedItem) {
-    $(clickedItem).toggleClass(activeClass);
-    updateSelectedProducts();
-  }
-
-
-  function removeProductFilters() {
-    if (isFiltered) {
-      $("." + filterTableClass + " .isFiltered").removeClass("isFiltered");
-      isFiltered = false;
-    }
-  }
-
-  function filterProductsMobile(filterString) {
-    if (isFiltered) {
-      removeProductFilters();
-    }
-    $(filterString).addClass("isFiltered");
-    isFiltered = true;
-  }
-
-  function initProdSlider() {
-    sageCompareControl.moduleResultsList = $('.filter-module-results-list').slick({
-      dots: false,
-      infinite: false,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      arrows: false,
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            focusOnSelect: true,
-            centerMode: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            initialSlide: 1
-          }
+    function updateFilters(clickedFilter) {
+        var positionInArray = $.inArray(clickedFilter, activeFiltersArray);
+        if (positionInArray === -1) {
+            activeFiltersArray.push(clickedFilter);
+        } else {
+            activeFiltersArray.splice(positionInArray, 1);
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-    });
-  }
-
-  function updateSelectedProducts() {
-    var selectedAttributes = [];
-    $("." + moduleClass + " ." + filterButtonsClass + "." + activeClass).each(function () {
-      selectedAttributes.push($(this).attr("data-filter-attribute"));
-    });
-
-    $("." + moduleClass + " ." + productResultsListClass + " ." + productPanelClass).each(function () {
-      var productAttributes,
-        productAttributesArray,
-        productScore = 0;
-
-      productAttributes = $(this).attr("data-filter-attributes");
-      productAttributesArray = productAttributes.split("|");
-
-      //loop through selected attributes and add as score to data-score of product-panel
-      for (var i = selectedAttributes.length; i--;) {
-        var testProd = productAttributesArray.indexOf(selectedAttributes[i]);
-        if (testProd >= 0) {
-          productScore++;
-        }
-      }
-      $(this).attr('data-score', productScore);
-      if (productScore < selectedAttributes.length) {
-        $(this).addClass(productPanelDisabledClass);
-      } else {
-        $(this).removeClass(productPanelDisabledClass);
-      }
-
-    });
-
-    $("." + moduleClass).find(".filter-text-no-match,.filter-text-one-match,.filter-text-more-matches").addClass("hidden");
-
-    var numSelected = $("." + moduleClass + " ." + productResultsListClass + " ." + productPanelClass + ":not(." + productPanelDisabledClass + ")").length;
-
-    switch (numSelected) {
-      case 0:
-        $("." + moduleClass + " .filter-text-no-match").removeClass("hidden");
-        break;
-      case 1:
-        $("." + moduleClass + " .filter-text-one-match").removeClass("hidden");
-        break;
-      default:
-        $("." + moduleClass + " .filter-text-more-matches").removeClass("hidden");
-        break;
+        updateProducts();
     }
-  }
+
+    function updateCompare(clickedItem) {
+        $(clickedItem).toggleClass(activeClass);
+        $(clickedItem).closest(".product-panel").toggleClass(activeClass);
+        $(clickedItem).closest(".panel").next(".icon-select-text").toggleClass(activeClass);
+    }
+
+
+    function drawProducts() {
+        var tmpScore = 0,
+            recomended = 0,
+            mostPopular = 0,
+            exaxtMatch = true;
+        //Identify product wth highest score and render as recomended 
+        console.log(productsArray[0].id + " : " + productsArray[0].score + " - " + productsArray[1].id + " : " + productsArray[1].score + " - " + productsArray[2].id + " : " + productsArray[2].score);
+        for (var i = productsArray.length; i--;) {
+            if (productsArray[i].score !== Math.round(productsArray[i].score)) {
+                mostPopular = i;
+            }
+            if (productsArray[i].score > tmpScore) {
+                tmpScore = productsArray[i].score;
+                recomended = i;
+            } else if (productsArray[i].score === tmpScore) {
+                tmpScore = productsArray[i].score;
+                recomended = i;
+                exaxtMatch = false; 
+            }
+        }
+
+        $("." + moduleClass + " .product-panel .panel-pre-heading").html("");
+        $("." + moduleClass + " [data-mob-prod-id]").removeClass("most-popular recommended");
+        $("." + moduleClass + " [data-desktop-prod-id]").removeClass("most-popular recommended");
+        if (tmpScore <= 0.5) {
+            $("." + moduleClass + " [data-desktop-prod-id=" + productsArray[mostPopular].id + "]").addClass("most-popular");
+            $("." + moduleClass + " [data-mob-prod-id=" + productsArray[mostPopular].id + "]").addClass("most-popular");
+            $("." + moduleClass + " .product-panel[data-mob-prod-id=" + productsArray[mostPopular].id + "] .panel-pre-heading").html("Most popular");
+        } else {
+            $("." + moduleClass + " [data-desktop-prod-id=" + productsArray[recomended].id + "]").addClass("recommended");
+            $("." + moduleClass + " [data-mob-prod-id=" + productsArray[recomended].id + "]").addClass("recommended");
+            $("." + moduleClass + " .product-panel[data-mob-prod-id=" + productsArray[recomended].id + "] .panel-pre-heading").html("Recommended");
+        }
+
+        console.log("mostPopular: " + productsArray[mostPopular].id);
+        console.log("recomended:" + productsArray[recomended].id);
+        
+    }
+
+    function calculateProductScore(tempFeaturesArray) {
+        var score = 0;
+        //console.log("clicked filter: " + activeFiltersArray);
+        //console.log("features: " + tempFeaturesArray);
+        //console.log("tmpLength: " + activeFiltersArray.length);
+        for (var i = activeFiltersArray.length; i--;) {
+            if (tempFeaturesArray.indexOf(activeFiltersArray[i]) !== -1) {
+                score++;
+            }
+        }
+        //console.log("score: " + score);
+        return score;
+    }
+
+    function updateProducts() {
+        productsArray = [];
+        $("." + moduleClass + " .product-panel").each(function () {
+            var tmpFeatures = $(this).attr("data-filter-attributes").split("|");
+            productsArray.push(
+            {
+                "id": $(this).attr("data-mob-prod-id"),
+                "score": Number($(this).attr("data-filter-product-score")) + calculateProductScore(tmpFeatures),
+                "features": tmpFeatures
+            });
+        });
+        drawProducts();
+    }
+
+    function initProdSlider() {
+        $(".filter-module-results-list").slick({
+          dots: false,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+          responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                focusOnSelect: true,
+                centerMode: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                initialSlide: 1
+              }
+            }
+          ]
+        });
+    }
 
   return {
     init: init
